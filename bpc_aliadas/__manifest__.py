db9603f7c6f0f1934572f8cdcbd0091eaea54785
jhonny16 <tinny.1609@gmail.com>
2023-01-25 15:18:10 -0500

    aliadas v:15.0.13.12

--- bpc_aliadas/__manifest__.py
+++ bpc_aliadas/__manifest__.py
@@ -10,10 +10,11 @@
     'company': 'BPC LATAM',
     'website': "https://www.bpc-lat.com/",
     'category': 'Sales',
-    'version': '15.0.13.11',
+    'version': '15.0.13.12',
     # any module necessary for this one to work correctly
     'depends': ['base','hn_einvoice', 'purchase_requisition','purchase','sale','sale_renting','sale_subscription','account_budget','sales_team',
-                'purchase_stock', 'approvals', 'purchase_requisition_stock','crm_helpdesk','website_helpdesk_form', 'hr'],
+                'purchase_stock', 'approvals', 'purchase_requisition_stock','crm_helpdesk','website_helpdesk_form', 'hr',
+                'helpdesk_stock'],
     # always loaded
     'data': [
         #security
